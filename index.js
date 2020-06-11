81188dd6b4d8dd70e1f2dfdb8f132784565a1ac9
James <jmcelroy@plexustech.net>
2020-06-11 14:08:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+var num1 = 31;
+
+var num2 = 2; 
+var multiply = num1 *= num2;
+
+var random = Math.floor(Math.random () * 10) + 1;
+
+var mod = 32 % 7
+
+var max = Math.max(17, 18, 19, 20)
